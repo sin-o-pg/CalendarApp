589bca71dd3d2a3b3c2ee623c32be2241e931890
sin <shinjiochiai1985@gmail.com>
2020-06-18 12:46:51 +0900

    カレンダー表示

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CalendarController extends Controller
 {
+
+
     public function index(){
-    	return view('calendar.index');
+    	$year = date('Y');
+    	$currentMonth = $month = date('m');
+    	$dates = self::getCalendarDates($year, $month);
+    	return view('calendar.index',compact('dates','currentMonth'));
+    }
+
+    private function getCalendarDates($year, $month)
+    {
+        $dateStr = sprintf('%04d-%02d-01', $year, $month);
+        $date = new Carbon($dateStr);
+        // カレンダーを四角形にするため、前月となる左上の隙間用のデータを入れるためずらす
+        $date->subDay($date->dayOfWeek);
+        // 同上。右下の隙間のための計算。
+        $count = 31 + $date->dayOfWeek;
+        $count = ceil($count / 7) * 7;
+        $dates = [];
+
+        for ($i = 0; $i < $count; $i++, $date->addDay()) {
+            // copyしないと全部同じオブジェクトを入れてしまうことになる
+            $dates[] = $date->copy();
+        }
+
+        return $dates;
     }
 }
