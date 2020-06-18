3fbd4a50d30fe385b697131c7b1c5c65ae0e31e7
sin <shinjiochiai1985@gmail.com>
2020-06-18 15:24:03 +0900

    月末日曜日対応

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -18,12 +18,13 @@ class CalendarController extends Controller
 
     private function getCalendarDates($year, $month)
     {
-        $dateStr = sprintf('%04d-%02d-01', $year, $month);
-        $date = new Carbon($dateStr);
+        // $dateStr = sprintf('%04d-%02d-01', $year, $month);
+        $date = new Carbon("{$year}-{$month}-01");
         // カレンダーを四角形にするため、前月となる左上の隙間用のデータを入れるためずらす
+        $addday = ($date->copy()->endOfMonth()->isSunday()) ? 7 : 0;
         $date->subDay($date->dayOfWeek);
         // 同上。右下の隙間のための計算。
-        $count = 31 + $date->dayOfWeek;
+        $count = 31 + $addday + $date->dayOfWeek;
         $count = ceil($count / 7) * 7;
         $dates = [];
 
