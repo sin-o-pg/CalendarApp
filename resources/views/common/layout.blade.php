ad9764f677293f4c1c85b443d05278bf00fffbf2
sin <shinjiochiai1985@gmail.com>
2020-06-18 15:02:55 +0900

    デザイン適用

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -5,6 +5,18 @@
     <title>マイカレンダー</title>
 
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<!-- Scripts -->
+	<script src="{{ asset('js/app.js') }}" defer></script>
+
+	<!-- Fonts -->
+	<link rel="dns-prefetch" href="//fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container py-4">
